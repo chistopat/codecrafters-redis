2ca405d08c528bc5baa042f0e14ac20a33eed50a
Egor Chistyakov <egchistyakov@ozon.ru>
2022-08-10 21:52:18 +0300

    use scanner

--- app/server.go
+++ app/server.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
@@ -23,11 +23,11 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go Handle(conn)
+		go Serve(conn)
 	}
 }
 
-func Handle(conn net.Conn) {
+func Serve(conn net.Conn) {
 	defer func(conn net.Conn) {
 		err := conn.Close()
 		if err != nil {
@@ -37,21 +37,49 @@ func Handle(conn net.Conn) {
 	}(conn)
 
 	reader := bufio.NewReader(conn)
-	for {
-		_, err := reader.ReadString('\n')
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("failed to read data, err:", err)
-			}
-			return
-		}
+	writer := bufio.NewWriter(conn)
+	Handle(reader, writer)
+}
+
+func Handle(in *bufio.Reader, out *bufio.Writer) {
+	scanner := bufio.NewScanner(in)
+	scanner.Split(ScanCRLF)
+	for scanner.Scan() {
+		token := scanner.Text()
 		//row = strings.TrimSpace(row)
 		//if row != Ping {
 		//	continue
 		//}
-		_, err = conn.Write([]byte("+PONG\r\n"))
+		fmt.Println(token)
+		_, err := out.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			return
 		}
+		out.Flush()
 	}
+}
+
+// https://stackoverflow.com/questions/37530451/golang-bufio-read-multiline-until-crlf-r-n-delimiter
+// dropCR drops a terminal \r from the data.
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
+}
+
+func ScanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.Index(data, []byte{'\r', '\n'}); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 2, dropCR(data[0:i]), nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), dropCR(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
