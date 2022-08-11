e49bd43c15bc00a8fe76f187b7b8fa27864bc86f
Egor Chistyakov <egchistyakov@ozon.ru>
2022-08-11 16:32:15 +0300

    try stage 6

--- app/redis/redis.go
+++ app/redis/redis.go
@@ -1,17 +1,24 @@
 package redis
 
-import "bufio"
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
 
 type MyRedis struct {
+	storage map[string]string
 }
 
 func NewMyRedis() *MyRedis {
-	return &MyRedis{}
+	return &MyRedis{
+		storage: map[string]string{},
+	}
 }
 
 func (r *MyRedis) OnConnect(in *bufio.Reader, out *bufio.Writer) {
 	for {
-		command, err := r.parseInput(in)
+		command, err := r.ParseInput(in)
 		if err != nil {
 			continue
 		}
@@ -20,9 +27,9 @@ func (r *MyRedis) OnConnect(in *bufio.Reader, out *bufio.Writer) {
 	}
 }
 
-func (r *MyRedis) parseInput(in *bufio.Reader) ([]string, error) {
-	scanner := bufio.NewScanner(in)
-	return nil, nil
+func (r *MyRedis) ParseInput(in *bufio.Reader) ([]string, error) {
+	parser := NewResp(in)
+	return parser.ParseArray()
 }
 
 func (r *MyRedis) Send(data []byte, out *bufio.Writer) {
@@ -36,5 +43,38 @@ func (r *MyRedis) Send(data []byte, out *bufio.Writer) {
 }
 
 func (r *MyRedis) Invoke(command []string) []byte {
-	return nil
+	method := command[0]
+	switch strings.ToUpper(method) {
+	case "PING":
+		return r.Ping()
+	case "ECHO":
+		return r.Echo([]byte(command[1]))
+	case "SET":
+		return r.Set(command[1], command[2])
+	case "GET":
+		return r.Get(command[1])
+	default:
+		return nil
+	}
+}
+
+func (r *MyRedis) Echo(data []byte) []byte {
+	return []byte(fmt.Sprintf("+%s\r\n", data))
+}
+
+func (r *MyRedis) Ping() []byte {
+	return []byte(fmt.Sprintf("+PONG\r\n"))
+}
+
+func (r *MyRedis) Set(key string, value string) []byte {
+	r.storage[key] = value
+	return []byte("+OK\r\n")
+}
+
+func (r *MyRedis) Get(key string) []byte {
+	if value, ok := r.storage[key]; !ok {
+		return []byte("(nil)\r\n")
+	} else {
+		return []byte(fmt.Sprintf("%s\r\n", value))
+	}
 }
--- app/redis/resp.go
+++ app/redis/resp.go
@@ -1,1 +1,53 @@
 package redis
+
+import (
+	"bufio"
+	"fmt"
+)
+
+type Resp struct {
+	s *bufio.Scanner
+}
+
+func NewResp(in *bufio.Reader) Resp {
+	scanner := bufio.NewScanner(in)
+	scanner.Split(ScanCRLF)
+	return Resp{
+		s: scanner,
+	}
+}
+
+func (r *Resp) ParseArray() ([]string, error) {
+	n, err := r.GetArrayLen(r.NextToken())
+	if err != nil {
+		return nil, err
+	}
+	results := make([]string, 0, n)
+	for n != 0 {
+		token := r.NextToken()
+		if NeedSkip(token) {
+			continue
+		}
+		results = append(results, token)
+	}
+	return results, nil
+}
+
+func (r *Resp) GetArrayLen(token string) (int, error) {
+	if len(token) == 2 && token[0] == '*' {
+		return int(token[1]-'0') * 2, nil
+	}
+	return -1, fmt.Errorf("invalid array token: %s", token)
+}
+
+func (r *Resp) NextToken() string {
+	r.s.Scan()
+	return r.s.Text()
+}
+
+func NeedSkip(token string) bool {
+	if len(token) > 0 && token[0] == '$' {
+		return true
+	}
+	return false
+}
