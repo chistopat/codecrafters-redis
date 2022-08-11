959362e0bc37ec69e78fdda616efe4629dbae7bb
Egor Chistyakov <egchistyakov@ozon.ru>
2022-08-11 14:20:43 +0300

    Revert "try to 5"
    This reverts commit a7c046554151d4bee5b5e2fc7d494b7b49a0aaf5.

--- app/server.go
+++ app/server.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -23,11 +23,11 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go Serve(conn)
+		go Handle(conn)
 	}
 }
 
-func Serve(conn net.Conn) {
+func Handle(conn net.Conn) {
 	defer func(conn net.Conn) {
 		err := conn.Close()
 		if err != nil {
@@ -37,65 +37,19 @@ func Serve(conn net.Conn) {
 	}(conn)
 
 	reader := bufio.NewReader(conn)
-	writer := bufio.NewWriter(conn)
-	Handle(reader, writer)
-}
-
-func Handle(in *bufio.Reader, out *bufio.Writer) {
-	scanner := bufio.NewScanner(in)
-	scanner.Split(ScanCRLF)
-	for scanner.Scan() {
-		token := scanner.Text()
-		switch {
-		case token == "ping":
-			out.Write([]byte("+PONG\r\n"))
-		case token == "echo":
-			scanner.Scan()
-			scanner.Scan()
-			out.Write([]byte(fmt.Sprintf("+%s\r\n", scanner.Text())))
-		default:
-			continue
+	for {
+		row, err := reader.ReadBytes('\n')
+		fmt.Println(row)
+		fmt.Println(string(row))
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("failed to read data, err:", err)
+			}
+			return
 		}
-		out.Flush()
-	}
-}
-
-func ParseArray(n int, scan *bufio.Scanner) []string {
-	result := make([]string, 0, n)
-	n *= 2
-	for n != 0 {
-		n--
-		scan.Scan()
-		token := scan.Text()
-		if rune(token[0]) == '$' {
-			continue
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			return
 		}
-		result = append(result, token)
 	}
-	return result
-}
-
-// https://stackoverflow.com/questions/37530451/golang-bufio-read-multiline-until-crlf-r-n-delimiter
-// dropCR drops a terminal \r from the data.
-func dropCR(data []byte) []byte {
-	if len(data) > 0 && data[len(data)-1] == '\r' {
-		return data[0 : len(data)-1]
-	}
-	return data
-}
-
-func ScanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-	if i := bytes.Index(data, []byte{'\r', '\n'}); i >= 0 {
-		// We have a full newline-terminated line.
-		return i + 2, dropCR(data[0:i]), nil
-	}
-	// If we're at EOF, we have a final, non-terminated line. Return it.
-	if atEOF {
-		return len(data), dropCR(data), nil
-	}
-	// Request more data.
-	return 0, nil, nil
 }
